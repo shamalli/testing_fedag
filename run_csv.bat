63daaa2c87580a9b7ccfb248088c2ca35ee155b8
Mihail Chepovskiy <contact@salephpscripts.com>
2025-06-23 23:50:16 +0600

    Iitial release

---
+++ run_csv.bat
@@ -0,0 +1,9 @@
+cd C:\Users\Shamalli\Desktop\NodeJS\testing_fedag
+git init
+git add .
+git remote add origin https://github.com/shamalli/testing_fedag
+git branch -M main
+
+git commit -m "Iitial release"
+git push -u origin main
+PAUSE
