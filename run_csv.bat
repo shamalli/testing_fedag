641b3e56f473418a164aff9ee4e8b47b8166934f
Mihail Chepovskiy <contact@salephpscripts.com>
2025-06-23 23:58:58 +0600

    Update

--- run_csv.bat
+++
@@ -1,9 +0,0 @@
-cd C:\Users\Shamalli\Desktop\NodeJS\testing_fedag
-git init
-git add .
-git remote add origin https://github.com/shamalli/testing_fedag
-git branch -M main
-
-git commit -m "Update"
-git push -u origin main
-PAUSE
